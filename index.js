d2fb3f0cbea66e15e0968304f2915efe36c07447
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-06 23:58:40 +0300

    commit tree 1686b8c5de9652b1b5f3c313fa355329add8b37e

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+// Multiply Function 
+
+let num1 ;
+let num2 ;
+let multiply ;
+
+multiply = num1 * num2 ;
+multiply = 62;
+
+//Math randomizer 
+
+let num10;
+num10 > 0;
+Math.floor(Math.random(numb10) * 10) + 1;
+
+// Mod  
+
+let mod;
+let num3;
+let num4;
+
+mod = num3 % num4 ;
+mod =4;
+
+// Max value 
+
+let max ;
+max = 20;
+max = Math.max();
